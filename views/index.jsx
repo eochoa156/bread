648512b6f9223e57b5d71ba144aa5982fc7d93ac
Emily Ochoa <eochoa156@gmail.com>
2024-03-05 21:10:08 -0800

    adding the bread images

--- views/index.jsx
+++ views/index.jsx
@@ -1,18 +1,28 @@
-const React = require('react') 
-const Default = require('./layouts/default') 
+const React = require('react')
+const Default = require('./layouts/default')
 
-function Index({ breads }) { 
+function Index({ breads }) {
     const display = breads.map((bread, i) => {
-        return <li key={i}>{bread.name}</li>
+        return (
+        <li key={i}>
+          <a href={`/bread/${i}`}>{bread.name}</a>
+        </li>
+        )
     })
-    return ( 
-        <Default> 
-            <h2>Index Page</h2>
-            <ul>
-                {display}
-            </ul> 
-        </Default> 
-    ) 
-} 
+    return (
+      <Default>
+        <h2>Index Page</h2>
+        <ul>
+            <div className="newButton">
+                <a href="/bread/new"><button>Add a new bread</button></a>
+            </div>
+            <div className="backButton">
+                <a href="/bread"><button>Go back to the index</button></a>
+            </div>
+            {display}
+        </ul>
+      </Default>
+    )
+}
 
 module.exports = Index
